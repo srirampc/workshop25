d7ec0bdd2f9b2796c9a38d9a67b0892e48a3348c
Sriram P C <sriram.pc@gmail.com>
2025-04-07 17:19:03 -0400

    Initial version of marimo notebook

---
+++ mogouwens.py
@@ -0,0 +1,81 @@
+import marimo
+
+__generated_with = "0.12.4"
+app = marimo.App(width="full")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import polars as pl
+    import duckdb
+    return duckdb, mo, pl
+
+
+@app.cell
+def _(duckdb, pl):
+    import os.path
+
+    def fetch_data():
+        lines_data = "https://static-content.springer.com/esm/art%3A10.1038%2Fs41593-019-0417-0/MediaObjects/41593_2019_417_MOESM4_ESM.xlsx",
+        types_data = "https://static-content.springer.com/esm/art%3A10.1038%2Fs41593-019-0417-0/MediaObjects/41593_2019_417_MOESM5_ESM.xlsx"
+        cdf = pl.read_excel(lines_data)
+        rdf = cdf.slice(offset=1, length=32)
+        rdf.columns = list(cdf.row(0))
+        gdf = cdf.slice(offset=35, length=5)
+        gdf.columns = list(cdf.row(34))
+        mdf = pl.read_excel(types_data)
+        return rdf, gdf, mdf
+
+    def save_db():
+        rdf, gdf, mdf = fetch_data()
+        with duckdb.connect("gouwens.db") as conn:
+            conn.execute(
+                "CREATE OR REPLACE TABLE driver_lines AS SELECT * FROM rdf"
+            )
+            conn.execute(
+                "CREATE OR REPLACE TABLE reporter_lines AS SELECT * FROM gdf"
+            )
+            conn.execute(
+                "CREATE OR REPLACE TABLE me_types AS SELECT * FROM mdf"
+            )
+            conn.commit()
+
+    if not os.path.exists("gouwens.db"):
+        save_db()
+    return fetch_data, os, save_db
+
+
+@app.cell
+def _(duckdb):
+    db_conn = duckdb.connect("./gouwens.db")
+    return (db_conn,)
+
+
+@app.cell
+def _(db_conn, me_types, mo):
+    import quak
+    mdf = mo.sql(
+        f"""
+        SELECT * FROM me_types
+        """,
+        engine=db_conn,
+    ) 
+    quak.Widget(mdf)
+    return mdf, quak
+
+
+@app.cell
+def _(db_conn, driver_lines, mo):
+    drdf = mo.sql(
+        f"""
+        SELECT * FROM driver_lines
+        """,
+        engine=db_conn,
+    )
+    drdf
+    return (drdf,)
+
+
+if __name__ == "__main__":
+    app.run()
